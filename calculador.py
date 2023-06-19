662b5eee084db34f769f348f16cd16e202113fc9
lsfc02 <linconsfc02@gmail.com>
2023-06-19 15:59:18 -0300

    Create calculador.py

---
+++ calculador.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite a nota da prova: '))
+n2 = float(input('Digite a nota das atividades: '))
+media = (n1 * 0.6) + (n2 * 0.4)
+print('media: ',media)
+if media<=4.9:
+    print('F')
+elif media<10:
+    print('Passou, Parabéns!!')
