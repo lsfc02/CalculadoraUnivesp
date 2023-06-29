b8df31ac23c5822954ddb311c37a8e0109e7f238
lsfc02 <137105091+lsfc02@users.noreply.github.com>
2023-06-29 14:27:21 -0300

    Update calculador.py

--- calculador.py
+++ calculador.py
@@ -3,6 +3,6 @@ n2 = float(input('Digite a nota das atividades: '))
 media = (n1 * 0.6) + (n2 * 0.4)
 print('media: ',media)
 if media<=4.9:
-    print('F')
+    print('Exame')
 elif media<10:
     print('Passou, Parabéns!!')
